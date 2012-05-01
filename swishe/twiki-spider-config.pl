37543509828a390c07f641f02812b9ad406a9fc1
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/SearchEngineSwishEAddOn@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- swishe/twiki-spider-config.pl
+++ swishe/twiki-spider-config.pl
@@ -8,18 +8,19 @@ sub twiki_url_tester {
     my $uri = shift;
 
     # don't index WebChanges or other pages that list everything
-    return 0 if $uri->path =~ /^(WebIndex|WebTopicList|TopicMap|WebChanges|WebRss)$/;
+    return 0
+      if $uri->path =~ /^(WebIndex|WebTopicList|TopicMap|WebChanges|WebRss)$/;
 
     # allow all views except ones with any arguments like sortcol
-    if ( ( $uri->path =~ /\/view(auth)?\// ) &&
-         (! ( $uri->query() )))
+    if (   ( $uri->path =~ /\/view(auth)?\// )
+        && ( !( $uri->query() ) ) )
     {
         return 1;
     }
 
     # allow attachment viewing, but not history or other args
-    if ( ( $uri->path =~ /\/pub\// ) &&
-         (! ( $uri->query() )))
+    if (   ( $uri->path =~ /\/pub\// )
+        && ( !( $uri->query() ) ) )
     {
         return 1;
     }
@@ -29,26 +30,28 @@ sub twiki_url_tester {
 }
 
 # reuse default filtering that includes support for .XLS handling (if Spreadsheet modules installed!)
-my ($filter_sub, $response_sub) = swish_filter();
+my ( $filter_sub, $response_sub ) = swish_filter();
 
 # See twiki/lib/TWiki.cfg
 #   base_url must be the same as ${defaultUrlHost}${scriptUrlPath}${dispViePath} from that file
 my %twiki = (
-	base_url    => 'http://twiki.corp.purisma.com/do/view/',
+    base_url    => 'http://twiki.corp.purisma.com/do/view/',
     email       => 'sknutson@purisma.com',
-	max_indexed => 15000,
+    max_indexed => 15000,
+
     # delay only 1 sec after filter rejects a topic
 
     # uncomment next line for testing
     #  debug => 'info,url',
-    delay_sec   => 0,
-	keep_alive  => 1,
-    # KEY BIT: this Twiki user must have "plain" template set up!
-	credentials => 'SpideringEngine:easy',
-    test_url    => \&twiki_url_tester,
-    test_response   => $response_sub,
-    filter_content  => $filter_sub,
-   );
+    delay_sec  => 0,
+    keep_alive => 1,
 
- @servers = ( \%twiki );
+    # KEY BIT: this Twiki user must have "plain" template set up!
+    credentials    => 'SpideringEngine:easy',
+    test_url       => \&twiki_url_tester,
+    test_response  => $response_sub,
+    filter_content => $filter_sub,
+);
+
+@servers = ( \%twiki );
 1;
