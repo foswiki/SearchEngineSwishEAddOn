48a6e66437807735f8c312cd92aacc6c6c06498b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SearchEngineSwishEAddOn@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- swishe/TemplateDefault.pm
+++ swishe/TemplateDefault.pm
@@ -10,24 +10,22 @@ use CGI;
 sub show_template {
     my ( $class, $template_params, $results ) = @_;
 
-
     my $q = $results->CGI;
 
-    my $output =  $q->header . page_header( $results );
+    my $output = $q->header . page_header($results);
 
     # Show form at top always
-    $output .= show_form( $results );
-
+    $output .= show_form($results);
 
     if ( $results->results ) {
-        $output .=  results_header( $results );
-        $output .=  show_result( $results, $_ ) for @{ $results->results };
+        $output .= results_header($results);
+        $output .= show_result( $results, $_ ) for @{ $results->results };
     }
 
     # Form after results (or at top if no results)
     #$output .= show_form( $results );
 
-    $output .= footer( $results );
+    $output .= footer($results);
 
     print $output;
 
@@ -38,29 +36,29 @@ sub show_template {
 
 sub page_header {
     my $results = shift;
-    my $title = $results->config('title') || 'Search our site with Swish-e';
+    my $title   = $results->config('title') || 'Search our site with Swish-e';
     my $message = $results->errstr;
 
-    $message = $message
-        ? qq[<br><font color="red">$message</font>]
-        : '' ;
-
+    $message =
+      $message
+      ? qq[<br><font color="red">$message</font>]
+      : '';
 
     my $html_title = $results->results
-        ? ( $results->navigation('hits')
-            . ' Results for ['
-            . CGI::escapeHTML( $results->{query_simple} )
-            . ']'
-           )
+      ? (   $results->navigation('hits')
+          . ' Results for ['
+          . CGI::escapeHTML( $results->{query_simple} )
+          . ']' )
 
-        : ( $results->errstr || $title );
+      : ( $results->errstr || $title );
 
-    my $default_logo = '<a href="http://swish-e.org"><img border="0" alt="Swish-e home page" src="http://swish-e.org/Images/swish-e.gif"></a> ' ;
+    my $default_logo =
+'<a href="http://swish-e.org"><img border="0" alt="Swish-e home page" src="http://swish-e.org/Images/swish-e.gif"></a> ';
 
-
-    my $logo = $results->config('on_intranet')
-               ? ''
-               : $results->config('logo') || $default_logo;
+    my $logo =
+      $results->config('on_intranet')
+      ? ''
+      : $results->config('logo') || $default_logo;
 
     return <<EOF;
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -95,30 +93,29 @@ EOF
 sub show_form {
 
     my $results = shift;
-    my $q = $results->{q};
-
+    my $q       = $results->{q};
 
     my $query = $q->param('query') || '';
 
-    $query = CGI::escapeHTML( $query );  # May contain quotes
-
+    $query = CGI::escapeHTML($query);    # May contain quotes
 
     # Here's some form components
-    
-    my $meta_select_list    = get_meta_name_limits( $results );
-    my $sorts               = get_sort_select_list( $results );
-    my $select_index        = get_index_select_list( $results );
-    my $limit_select        = get_limit_select( $results );
-    
-    my $date_ranges_select  = $results->get_date_ranges;
+
+    my $meta_select_list = get_meta_name_limits($results);
+    my $sorts            = get_sort_select_list($results);
+    my $select_index     = get_index_select_list($results);
+    my $limit_select     = get_limit_select($results);
+
+    my $date_ranges_select = $results->get_date_ranges;
 
     my $form = $q->script_name;
 
-    my $advanced_link = qq[<small><a href="$form">advanced form</a></small>]; 
+    my $advanced_link = qq[<small><a href="$form">advanced form</a></small>];
 
-    my $advanced_form = $q->param('brief')
-                        ? $advanced_link
-                        : <<EOF;
+    my $advanced_form =
+        $q->param('brief')
+      ? $advanced_link
+      : <<EOF;
         $meta_select_list
         $sorts
         $select_index
@@ -127,11 +124,11 @@ sub show_form {
 EOF
 
     my $extra = $results->config('extra_fields');
-    my $hidden = !$extra ? ''
-                 : join "\n", map { $q->hidden($_) } @$extra; 
+    my $hidden =
+      !$extra
+      ? ''
+      : join "\n", map { $q->hidden($_) } @$extra;
 
-
-    
     return <<EOF;
 <table>
 <tr><td>
@@ -159,7 +156,6 @@ EOF
 EOF
 }
 
-
 #=====================================================================
 # This routine creates the results header display
 # and navigation bar
@@ -170,28 +166,27 @@ EOF
 sub results_header {
 
     my $results = shift;
-    my $config = $results->{config};
-    my $q = $results->{q};
+    my $config  = $results->{config};
+    my $q       = $results->{q};
 
-
-
-    my $swr = $results->header('removed stopwords');
+    my $swr       = $results->header('removed stopwords');
     my $stopwords = '';
 
-
     if ( $swr && ref $swr eq 'ARRAY' ) {
-        $stopwords = @$swr > 1
-        ? join( ', ', map { "<b>$_</b>" } @$swr ) . ' are very common words and were not included in your search'
-        : join( ', ', map { "<b>$_</b>" } @$swr ) . ' is a very common word and was not included in your search';
+        $stopwords =
+          @$swr > 1
+          ? join( ', ', map { "<b>$_</b>" } @$swr )
+          . ' are very common words and were not included in your search'
+          : join( ', ', map { "<b>$_</b>" } @$swr )
+          . ' is a very common word and was not included in your search';
     }
 
     my $limits = '';
 
     #  Ok, this is ugly.
 
-
     if ( $results->{DateRanges_time_low} && $results->{DateRanges_time_high} ) {
-        my $low = scalar localtime $results->{DateRanges_time_low};
+        my $low  = scalar localtime $results->{DateRanges_time_low};
         my $high = scalar localtime $results->{DateRanges_time_high};
         $limits = <<EOF;
         <tr>
@@ -204,48 +199,46 @@ sub results_header {
 EOF
     }
 
-    my $query_href = $results->{query_href};
+    my $query_href   = $results->{query_href};
     my $query_simple = CGI::escapeHTML( $results->{query_simple} );
-    my $pages       = $results->navigation('pages');
+    my $pages        = $results->navigation('pages');
 
-    my $prev        = $results->navigation('prev');
-    my $prev_count  = $results->navigation('prev_count');
-    my $next        = $results->navigation('next');
-    my $next_count  = $results->navigation('next_count');
+    my $prev       = $results->navigation('prev');
+    my $prev_count = $results->navigation('prev_count');
+    my $next       = $results->navigation('next');
+    my $next_count = $results->navigation('next_count');
 
-    my $hits        = $results->navigation('hits');
-    my $from        = $results->navigation('from');
-    my $to          = $results->navigation('to');
+    my $hits = $results->navigation('hits');
+    my $from = $results->navigation('from');
+    my $to   = $results->navigation('to');
 
     my $run_time    = $results->navigation('run_time');
     my $search_time = $results->navigation('search_time');
 
-
-
-
-
     my $links = '';
 
-    $links .= '<font size="-1" face="Geneva, Arial, Helvetica, San-Serif">&nbsp;Page:</font>' . $pages
-        if $pages;
+    $links .=
+'<font size="-1" face="Geneva, Arial, Helvetica, San-Serif">&nbsp;Page:</font>'
+      . $pages
+      if $pages;
 
-    $links .= qq[ <a href="$query_href&amp;start=$prev">Previous $prev_count</a>]
-        if $prev_count;
+    $links .=
+      qq[ <a href="$query_href&amp;start=$prev">Previous $prev_count</a>]
+      if $prev_count;
 
     $links .= qq[ <a href="$query_href&amp;start=$next">Next $next_count</a>]
-        if $next_count;
-
+      if $next_count;
 
     # Save for the bottom of the screen.
     $results->{LINKS} = $links;
 
-    $links = qq[<tr><td colspan="2" bgcolor="#EEEEEE">$links</td></tr>] if $links;
+    $links = qq[<tr><td colspan="2" bgcolor="#EEEEEE">$links</td></tr>]
+      if $links;
 
-    $query_simple = $query_simple
-        ? "&nbsp;Results for <b>$query_simple</b>"
-        : '';
-
-
+    $query_simple =
+      $query_simple
+      ? "&nbsp;Results for <b>$query_simple</b>"
+      : '';
 
     return <<EOF;
 
@@ -280,50 +273,44 @@ EOF
 #
 #
 sub show_result {
-    my ($results, $this_result ) = @_;
+    my ( $results, $this_result ) = @_;
 
     my $conf = $results->{conf};
 
     my $DocTitle = $results->config('title_property') || 'swishtitle';
 
-
-    my $title = $this_result->{$DocTitle} || $this_result->{swishdocpath} || '?';
+    my $title =
+         $this_result->{$DocTitle}
+      || $this_result->{swishdocpath}
+      || '?';
 
     my $name_labels = $results->config('name_labels');
-
-
 
     # The the properties to display
 
     my $props = '';
 
     my $display_props = $results->config('display_props');
-    if ( $display_props ) {
+    if ($display_props) {
 
-
-        $props = join "\n",
-            '<br><table cellpadding="0" cellspacing="0">',
-            map ( {
-                '<tr><td><small>'
-                . ( $name_labels->{$_} || $_ )
-                . ':</small></td><td><small> '
-                . '<b>'
-                . ( defined $this_result->{$_} ?  $this_result->{$_} : '' ) 
-                . '</b>'
-                . '</small></td></tr>'
-                 }   @$display_props
-            ),
-            '</table>';
+        $props = join "\n", '<br><table cellpadding="0" cellspacing="0">',
+          map ( {
+                    '<tr><td><small>'
+                  . ( $name_labels->{$_} || $_ )
+                  . ':</small></td><td><small> ' . '<b>'
+                  . ( defined $this_result->{$_} ? $this_result->{$_} : '' )
+                  . '</b>'
+                  . '</small></td></tr>'
+            } @$display_props ),
+          '</table>';
     }
-
 
     my $description_prop = $results->config('description_prop');
 
     my $description = '';
-    if ( $description_prop ) {
-        $description = $this_result->{ $description_prop } || '';
+    if ($description_prop) {
+        $description = $this_result->{$description_prop} || '';
     }
-
 
     return <<EOF;
     <dl>
@@ -343,18 +330,18 @@ EOF
 #
 #
 
-
 sub footer {
     my $results = shift;
 
-    my $mod_perl = $ENV{MOD_PERL}
-               ? '<br><small>Response brought to you by <a href="http://perl.apache.org"><em>mod_perl</em></a></small>'
-               : '';
+    my $mod_perl =
+      $ENV{MOD_PERL}
+      ? '<br><small>Response brought to you by <a href="http://perl.apache.org"><em>mod_perl</em></a></small>'
+      : '';
 
-    my $valid_html_logo = $results->config('on_intranet')
-               ? ''
-               : '<p><a href="http://validator.w3.org/check/referer"><img border="0" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!" height="31" width="88"></a></p>';
-
+    my $valid_html_logo =
+      $results->config('on_intranet')
+      ? ''
+      : '<p><a href="http://validator.w3.org/check/referer"><img border="0" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!" height="31" width="88"></a></p>';
 
     my $links = $results->{LINKS} || '';
     return <<EOF;
@@ -381,59 +368,49 @@ EOF
 #==================================================================
 
 sub get_meta_name_limits {
-    my ( $results ) = @_;
+    my ($results) = @_;
 
     my $metanames = $results->config('metanames');
     return '' unless $metanames;
 
-
     my $name_labels = $results->config('name_labels');
-    my $q = $results->CGI;
+    my $q           = $results->CGI;
 
-
-    return join "\n",
-        'Limit search to:',
-        $q->radio_group(
-            -name   =>'metaname',
-            -values => $metanames,
-            -default=>$metanames->[0],
-            -labels =>$name_labels
-        ),
-        '<br>';
+    return join "\n", 'Limit search to:',
+      $q->radio_group(
+        -name    => 'metaname',
+        -values  => $metanames,
+        -default => $metanames->[0],
+        -labels  => $name_labels
+      ),
+      '<br>';
 }
 
 sub get_sort_select_list {
-    my ( $results ) = @_;
+    my ($results) = @_;
 
     my $sort_metas = $results->config('sorts');
     return '' unless $sort_metas;
 
-    
     my $name_labels = $results->config('name_labels');
-    my $q = $results->CGI;
+    my $q           = $results->CGI;
 
-
-
-    return join "\n",
-        'Sort by:',
-        $q->popup_menu(
-            -name   =>'sort',
-            -values => $sort_metas,
-            -default=>$sort_metas->[0],
-            -labels =>$name_labels
-        ),
-        $q->checkbox(
-            -name   => 'reverse',
-            -label  => 'Reverse Sort'
-        );
+    return join "\n", 'Sort by:',
+      $q->popup_menu(
+        -name    => 'sort',
+        -values  => $sort_metas,
+        -default => $sort_metas->[0],
+        -labels  => $name_labels
+      ),
+      $q->checkbox(
+        -name  => 'reverse',
+        -label => 'Reverse Sort'
+      );
 }
 
-
-
 sub get_index_select_list {
-    my ( $results ) = @_;
+    my ($results) = @_;
     my $q = $results->CGI;
-
 
     my $indexes = $results->config('swish_index');
     return '' unless ref $indexes eq 'ARRAY';
@@ -441,41 +418,40 @@ sub get_index_select_list {
     my $select_config = $results->config('select_indexes');
     return '' unless $select_config && ref $select_config eq 'HASH';
 
-
     # Should return a warning, as this might be a likely mistake
     # This jumps through hoops so that real index file name is not exposed
-    
-    return '' unless exists $select_config->{labels}
-              && ref $select_config->{labels} eq 'ARRAY'
-              && @$indexes == @{$select_config->{labels}};
 
+    return ''
+      unless exists $select_config->{labels}
+          && ref $select_config->{labels} eq 'ARRAY'
+          && @$indexes == @{ $select_config->{labels} };
 
-    my @labels = @{$select_config->{labels}};
+    my @labels = @{ $select_config->{labels} };
     my %map;
 
-    for ( 0..$#labels ) {
+    for ( 0 .. $#labels ) {
         $map{$_} = $labels[$_];
     }
 
     my $method = $select_config->{method} || 'checkbox_group';
-    my @cols = $select_config->{columns} ? ('-columns', $select_config->{columns}) : ();
+    my @cols =
+      $select_config->{columns}
+      ? ( '-columns', $select_config->{columns} )
+      : ();
 
-    return join "\n",
-        '<br>',
-        ( $select_config->{description} || 'Select: '),
-        $q->$method(
-        -name   => 'si',
-        -values => [0..$#labels],
-        -default=> 0,
-        -labels => \%map,
-        @cols );
+    return join "\n", '<br>', ( $select_config->{description} || 'Select: ' ),
+      $q->$method(
+        -name    => 'si',
+        -values  => [ 0 .. $#labels ],
+        -default => 0,
+        -labels  => \%map,
+        @cols
+      );
 }
 
-
 sub get_limit_select {
-    my ( $results ) = @_;
+    my ($results) = @_;
     my $q = $results->CGI;
-
 
     my $limit = $results->config('select_by_meta');
     return '' unless ref $limit eq 'HASH';
@@ -488,13 +464,12 @@ sub get_limit_select {
         -labels => $limit->{labels} || {},
     );
 
-    push @options, ( -columns=> $limit->{columns} ) if $limit->{columns};
-    
+    push @options, ( -columns => $limit->{columns} ) if $limit->{columns};
 
     return join "\n",
-        '<br>',
-        ( $limit->{description} || 'Select: '),
-        $q->$method( @options );
+      '<br>',
+      ( $limit->{description} || 'Select: ' ),
+      $q->$method(@options);
 }
 1;
 
